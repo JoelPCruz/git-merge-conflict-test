37b928ba09516264b49d32665e04d2774fe0f210
Joel Cruz <104873706+Joerican305@users.noreply.github.com>
2023-08-19 03:02:23 -0400

    changing variable names to sim a merge conflict

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 // MERGE CONFLICT TEST
 
 const createHtmlString = msg => `${msg}`;  
+class CreateHtmlElement {
+    constructor(htmlElement1, text1){
+        this.htmlElement1 = htmlElement1;
+        this.text1 = text1;
+        return `<${htmlElement1}>${text1}</${htmlElement1}>`;
+    }
+    createHtmlString(string1) {
+        return `${string1}`;
+    }
+}
+
+const newLiElement = new CreateHtmlElement("li","hello");
