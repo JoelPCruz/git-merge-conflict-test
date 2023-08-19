a14ba653e1e828e9d83db836be3a67a0d395babb
Joel Cruz <104873706+Joerican305@users.noreply.github.com>
2023-08-19 03:32:44 -0400

    updating function, testing out git pull

--- script.js
+++ script.js
@@ -1,31 +1,16 @@
 // MERGE CONFLICT TEST
 
-<<<<<<< HEAD
-const createHtmlString = msg => `${msg}`; 
 class CreateHtmlElement {
     constructor(htmlElement, text){
         this.htmlElement = htmlElement;
         this.text = text;
         return `<${htmlElement}>${text}</${htmlElement}>`;
     }
-    createHtmlString(string) {
-        return `${string}`;
+    createHtmlMathEquation(num1, num2, operator) {
+        return `${num1} ${operator} ${num2}`;
     }
 }
 
 const newLiElement = new CreateHtmlElement("li","hello"); 
-=======
-const createHtmlString = msg => `${msg}`;  
-class CreateHtmlElement {
-    constructor(htmlElement1, text1){
-        this.htmlElement1 = htmlElement1;
-        this.text1 = text1;
-        return `<${htmlElement1}>${text1}</${htmlElement1}>`;
-    }
-    createHtmlString(string1) {
-        return `${string1}`;
-    }
-}
 
-const newLiElement = new CreateHtmlElement("li","hello");
->>>>>>> test-branch2
+
