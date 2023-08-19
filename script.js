50648b7bae58943bd6ceec641bbce99f14421484
Joel Cruz <104873706+Joerican305@users.noreply.github.com>
2023-08-19 02:54:20 -0400

    updated function to match main branch

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 // MERGE CONFLICT TEST
 
-const createHtmlString = msg => `${msg}`;  
+const createHtmlString = str => `${str}`;  
+
+class CreateHtmlElement {
+    constructor(element, string){
+        this.element = element;
+        this.string = string;
+        return `<${element}>${string}</${element}>`;
+    }
+    createHtmlString(str) {
+        return `${str}`;
+    }
+}
+
+const newLiElement = new CreateHtmlElement("li","hello");
